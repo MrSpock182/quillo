a201ee65c1bf3811bee53e5a899dbd3b59d25dac
MrSpock182 <klebernunes182@gmail.com>
2023-09-13 00:35:45 -0300

    first commit

---
+++ src/main/java/io/github/mrspock182/Cnpj.java
@@ -0,0 +1,40 @@
+package io.github.mrspock182;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Constraint(
+        validatedBy = {CnpjValidator.class}
+)
+@Target({
+        ElementType.METHOD,
+        ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE,
+        ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER,
+        ElementType.TYPE_USE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Repeatable(Cnpj.List.class)
+public @interface Cnpj {
+    String message() default "CNPJ inválido";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({
+            ElementType.METHOD,
+            ElementType.FIELD,
+            ElementType.ANNOTATION_TYPE,
+            ElementType.CONSTRUCTOR,
+            ElementType.PARAMETER,
+            ElementType.TYPE_USE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        Cnpj[] value();
+    }
+}
